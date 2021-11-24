1a7a69e999ac8c669bdf667c9d182e83f1c34be4
Radu Margineanu <radu.margineanu@swisscom.com>
2021-11-24 18:25:34 +0100

    First implementation of chat store API endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"os"
 	"fmt"
+	"time"
 	"net/http"
+	"strconv"
 
 	"github.com/darthbanana13/datastorageapi/pkg/localpath"
+	arangoInit "github.com/darthbanana13/datastorageapi/pkg/initArangoDb"
 	initLog "github.com/darthbanana13/datastorageapi/pkg/initlogrusfromtext"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
+	driver "github.com/arangodb/go-driver"
 )
 
 type TempSaveData struct {
-  Text      string `json:"text"`
-  Language  string `json:"language"`
+  Text		    string `json:"text"`
+  Language	    string `json:"language"`
+  CustomerId	    int
+  DialogId	    int
+  Consent	    bool
+  NanoTimestamp     int64
 }
+
+var db driver.Database
 
 func init() {
   appDir, err := localpath.Get()
@@ -33,13 +43,30 @@ func init() {
   if err != nil {
     log.Fatalf("Error initializing log settings: %v", err)
   }
+
+  db, err = arangoInit.InitDbWith(
+    os.Getenv("ARANGODB_PROTOCOL") + os.Getenv("ARANGODB_HOST") + ":" + os.Getenv("ARANGODB_PORT"),
+    os.Getenv("ARANGODB_USER"),
+    os.Getenv("ARANGODB_PASSWORD"),
+    os.Getenv("ARANGODB_NAME"),
+    []string{"chat"},
+  )
 }
 
+func insertArangoDb(jsonData string) {
+  db.Query(nil, "INSERT" + jsonData + "INTO chat", nil)
+}
+
+//TODO: Return error if URL parameters are not int
 func tempSaveData(c *gin.Context) {
   var chatMessage TempSaveData
   if err := c.ShouldBindJSON(&chatMessage); err != nil {
     c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
   }
+  chatMessage.CustomerId, _ = strconv.Atoi(c.Param("customerId"))
+  chatMessage.DialogId, _ = strconv.Atoi(c.Param("dialogId"))
+  chatMessage.Consent = false
+  chatMessage.NanoTimestamp = time.Now().UnixNano()
   c.JSON(
     http.StatusOK,
     gin.H{
